90dd7a71b130f4c0f9a46eb9006c4d59954919a3
Shambhavi <34545868+tshambhavi@users.noreply.github.com>
2019-08-06 17:03:27 +0530

    Update meta_details.py

--- code/meta_details.py
+++ code/meta_details.py
@@ -1,36 +1,18 @@
 import json
-from html import unescape
-from collections import defaultdict
 
 
-def get_metadata(text_file):
+def get_metadata_artists(text_file):
     """
-    Extracts metadata about each painting from a relevant text file, stores the metadata by painting name in a dictionary, and returns the dictionary
+    Extracts the dictionary that has been saved in the meta.txt file for the selected artist
     """
 
-    meta_dict = defaultdict()
+    data_dict = {}
 
     try:
         with open(text_file, 'r') as input_file:
-            data_list = json.load(input_file)
+            data_dict = json.load(input_file)
     except Exception as e:
         print(e)
         raise e
 
-    for painting in data_list:
-        artist = unescape(painting['artistName'])
-        title = unescape(painting['title'])
-        year = unescape(painting['year'])
-        painting_url = unescape(painting["paintingUrl"])
-        meta_text = [artist, title, year, painting_url]
-
-        # Retrieve image name from link to image.
-        index = painting['image'].rfind('/')
-        image_name = painting['image'][index + 1:]
-
-        try:
-            meta_dict[image_name].append(meta_text)
-        except KeyError:
-            meta_dict[image_name] = (meta_text)
-
-    return meta_dict
+    return data_dict
