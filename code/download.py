aef2ada6f857ce20ac4b315c3dd235e9636196c4
Shambhavi <34545868+tshambhavi@users.noreply.github.com>
2019-08-06 16:22:33 +0530

    Update the download.py file

--- code/download.py
+++ code/download.py
@@ -1,192 +1,243 @@
 """
-This code queries WikiArt and returns data from the category that is chosen by the build_paths() functions.
+This code queries WikiArt and returns data categorized by artist name
 """
 
-from PIL import Image
-import requests
-import shutil
-import json
-from pathlib import Path
-from random import randrange
-from os import path, listdir
-from sys import exit
 import settings
+import word_list
 
-# from PIL import ImageFile
-# ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-number = randrange(9)
-FOLDER_NAME, CUSTOM_URL, ASSET_PATH, METADATA_FILE = settings.build_paths(
-    number)
+import re
+import json
+import requests
 
 
-def get_json():
-    """
-    Gets JSON data about a category of paintings from WikiArt, and returns a list of objects containing data specific to each painting
-    """
+from PIL import Image
+from html import unescape
+from wget import download
 
-    for page in range(1, 2):
-        data_list = []
-        url = CUSTOM_URL + str(page)
-        print(page, "pages processed")
+from sys import exit
+from pathlib import Path
+from os import path, listdir
+
+
+class WikiDownloader():
+    
+    def __init__(self, number):
+        '''
+        Uses the builds_paths() functions in the settings file to select an artist and generate paths and URLs pertaining to the artist
+        '''
+        self.FOLDER_NAME, self.CUSTOM_URL, self.ASSET_PATH, self.METADATA_FILE = settings.build_paths(
+            number)
+
+    def get_paths(self):
+        return self.METADATA_FILE
+
+    def substitute(self, text):
+        return re.sub(r'[\s]+', '-', text)
+
+    def create_meta_dict(self, title, artist, year, image_temp):
+        '''
+        Generates a dictionary that contains meta data about a painting of the chosen artist
+        
+        Format:
+        {<image_name>: [<title>, <artist>, <year>, <painting_url>, <image_url>]}
+        
+        Example:
+        {"landscape-1868.jpg": ["Landscape", "Pierre-Auguste Renoir", "1868", "/en/pierre-auguste-renoir/landscape-1868", 
+        "https://uploads7.wikiart.org/images/pierre-auguste-renoir/landscape-1868.jpg"],}
+        '''
+        meta_dict = {}
+        artist_url = (self.substitute(artist)).lower()
+
+        index = image_temp.rfind('!')
+        image_url = image_temp[:index]
+
+        index_slash = image_url.rfind('/')
+        image_name = image_url[index_slash + 1:]
+
+        index_dot = image_name.rfind('.')
+        image_name_minus_ext = image_name[:index_dot]
+        painting_url = f'''/en/{artist_url}/{image_name_minus_ext}'''
+
+        meta_text = [title, artist, year, painting_url, image_url]
+
+        try:
+            meta_dict[image_name].append(meta_text)
+        except KeyError:
+            meta_dict[image_name] = (meta_text)
+
+        return meta_dict
+
+    def get_json(self):
+        """
+        Gets JSON data about a specified number of paintings made by the artist who was chosen in the __init__() function
+        Returns a dictionary containing data about the paintings
+        """
+        data_dict = {}
+        json_url = self.CUSTOM_URL
         try:
             response = requests.get(
-                url, timeout=settings.METADATA_REQUEST_TIMEOUT)
-            # json_data = response.json()
-            if response.json()['Paintings']:
-                data_list = response.json()['Paintings']
+                json_url, timeout=settings.METADATA_REQUEST_TIMEOUT)
+
+            if response.json():
+                # Gets data about three paintings
+                # The range can be modified as per requirements
+                for number in range(0, 3):
+                    painting_data = response.json()[number]
+                    title = unescape(painting_data['title'])
+                    has_word = word_list.prune_title(title)
+                    if not has_word:
+                        artist = unescape(painting_data['artistName'])
+                        year = unescape(painting_data['yearAsString'])
+                        image_temp = unescape(painting_data['image'])
+                        meta_dict = self.create_meta_dict(
+                            title, artist, year, image_temp)
+                        data_dict.update(meta_dict)
+                    else:
+                        continue
         except requests.exceptions.RequestException as e:
             print(e)
             exit(1)
-    return data_list
+        return data_dict
 
+    def get_dict_from_file(self, text_file):
+        '''
+        Accepts a text file and, if the file exists and is not empty, extracts its elements into a dictionary
+        Returns the dictionary
+        '''
+        data_dict = {}
+        file = Path(text_file)
+        if file.is_file() and file.stat().st_size != 0:
+            try:
+                with open(text_file, 'r') as input_file:
+                    data_dict = json.load(input_file)
+            except Exception as e:
+                print(e)
+                raise e
+        return data_dict
 
-def prune(list):
-    '''
-    Removes list elements whose title variables contains specific words
-    '''
-    items_deleted_from_list = 0
-    word_list = ['sex', 'nude', 'nudist', 'bath', 'lake', 'river', 'sea', 'beach',
-                 'anachronism', 'man and woman', 'couple', 'aphrodite', 'anadyomene', 'venus', 'proposition', 'romance', 'embarrass', 'embarrassing']
-    for word in word_list:
-        if list:
-            for painting in list:
-                if word in painting["title"].lower():
-                    list.remove(painting)
-                    items_deleted_from_list += 1
+    def save_data(self, text_file, data_dict):
+        """
+        Saves the dictionary that is passed on to it as a text file
+        If the text file already exists and contains items, updates the file using the data in the dicitonary
+        """
+        existing_dict = self.get_dict_from_file(text_file)
+        existing_dict.update(data_dict)
+
+        with open(text_file, 'w+') as output_file:
+            json.dump(existing_dict, output_file)
+
+    def get_image_links(self, text_file):
+        """
+        Converts a text file into a dictionary, and extracts painting names and links from each element in the dictionary
+        """
+        painting_tuples = []
+
+        with open(text_file, 'r') as input_file:
+            data_dict = json.load(input_file)
+
+        for key in data_dict:
+            tuple = (key, data_dict[key][-1])
+            painting_tuples.append(tuple)
+
+        return painting_tuples
+
+    def download_images(self, tuple_list):
+        """
+        Accepts a list of painting names and links, downloads each painting, and saves it in a specified location
+        """
+
+        count = 0
+        download_folder = f'{self.ASSET_PATH}img_large\\'
+
+        downloaded_img_count = len([name for name in listdir(
+            download_folder) if path.isfile(path.join(download_folder, name))])
+
+        if len(tuple_list) == downloaded_img_count:
+            print('Nothing to download')
+        elif len(tuple_list) > downloaded_img_count:
+            print(
+                f'Going to download from {len(tuple_list) - downloaded_img_count} links')
         else:
-            break
-    print(f'{items_deleted_from_list} items deleted')
-    return list
+            print(
+                f'Going to download from {downloaded_img_count - len(tuple_list)} links')
 
+        for element in tuple_list:
+            image_name = element[0]
+            image_link = element[1]
+            file_location = f'{self.ASSET_PATH}img_large\\{image_name}'
 
-def get_list_from_file(text_file):
-    '''
-    If the file exists and is not empty, extracts its elements, adds these elements to a list, and returns the list
-    '''
-    data_list = []
-    file = Path(text_file)
-    if file.is_file() and file.stat().st_size != 0:
-        try:
-            with open(text_file, 'r') as input_file:
-                data_list = json.load(input_file)
-        except Exception as e:
-            print(e)
-            raise e
-    return data_list
-
-
-def save_data(text_file, data_list):
-    """
-    Saves the list of objects returned by get_json() and prune() as a text file
-    If the file already contains items, extends the list with these items and overwrites the file
-    """
-    existing_list = get_list_from_file(text_file)
-    data_list.extend(existing_list)
-
-    with open(text_file, 'w+') as output_file:
-        json.dump(data_list, output_file)
-
-
-def get_image_links(text_file):
-    """
-    Converts the text file into a list, and extracts image links from each object in the list
-    """
-    painting_links = []
-
-    with open(text_file, 'r') as input_file:
-        data_list = json.load(input_file)
-
-    for painting in data_list:
-        painting_links.append(painting['image'])
-
-    return painting_links
-
-
-def download_images(links):
-    """
-    Accepts a list of links to images, downloads each image, and saves it in a specified location
-    """
-    print(f'Going to download from {len(links)} links')
-    count = 0
-    for link in links:
-        image_name = link.rsplit('/', 1)[1]
-        file_location = f'{ASSET_PATH}img_large\\{image_name}'
-
-        if image_name.endswith(('.png', '.jpg', '.jpeg')):
-            if not path.isfile(file_location):
-                print("Processing", link)
-                try:
-                    response = requests.get(link,
-                                            timeout=settings.METADATA_REQUEST_TIMEOUT, stream=True)
-                except requests.exceptions.RequestException as e:
-                    print(e)
-                    return
-
-                with open(str(file_location), 'wb') as outfile:
-                    shutil.copyfileobj(response.raw, outfile)
-                    count += 1
-
-    print(f'Downloaded {count} photos')
-
-
-def resize_save_images():
-    """
-    Retrieves names of image files from a specified folder, resizes and compresses the image files and saves them in a separate folder
-    """
-
-    folder_path = f'{ASSET_PATH}img_large\\'
-    resized_folder_path = f'{ASSET_PATH}img\\'
-    images = listdir(folder_path)
-    max_size = 100
-
-    count_resized = 0
-    count_unresized = 0
-    count_saved = 0
-
-    for image in images:
-        if image.endswith(('.png', '.jpg', '.jpeg')):
-            image_path = folder_path + image
-            resized_image_path = resized_folder_path + image
-
-            if path.isfile(image_path):
-                if not path.isfile(resized_image_path):
-                    img = Image.open(image_path)
+            if image_name.endswith(('.png', '.jpg', '.jpeg')):
+                if not path.isfile(file_location):
+                    print(f"\nProcessing {image_name}")
                     try:
-                        if img.size[0] > 900:
-                            if path.getsize(image_path) > (max_size * 1024):
-                                basewidth = 900
-                                width_percent = (
-                                    basewidth / float(img.size[0]))
-                                horizontal_size = int(
-                                    (float(img.size[1]) * float(width_percent)))
-                                img = img.resize(
-                                    (basewidth, horizontal_size), Image.ANTIALIAS)
-                                count_resized += 1
-                    except:
-                        print(f'Could not resize {image}')
-                        count_unresized += 1
+                        download(image_link, file_location)
+                        count += 1
+                    except Exception as e:
+                        print(e)
+                        continue
+                
+        print(f'\nDownloaded {count} photos\n')
 
-                    if img.size[0] >= 440 and img.size[1] >= 440:
+    def resize_save_images(self):
+        """
+        Retrieves names of image files from a specified folder, resizes and compresses the image files, and saves them in a separate folder
+        """
+
+        folder_path = f'{self.ASSET_PATH}img_large\\'
+        resized_folder_path = f'{self.ASSET_PATH}img\\'
+        images = listdir(folder_path)
+        max_size = 100
+
+        count_resized = 0
+        count_unresized = 0
+        count_saved = 0
+
+        for image in images:
+            if image.endswith(('.png', '.jpg', '.jpeg')):
+                image_path = folder_path + image
+                resized_image_path = resized_folder_path + image
+
+                if path.isfile(image_path):
+                    if not path.isfile(resized_image_path):
+                        img = Image.open(image_path)
                         try:
-                            img.save(resized_image_path,
-                                     quality=85, optimize=True)
-                            count_saved += 1
+                            if img.size[0] > 1024:
+                                if path.getsize(image_path) > (max_size * 1024):
+                                    basewidth = 1024
+                                    width_percent = (
+                                        basewidth / float(img.size[0]))
+                                    horizontal_size = int(
+                                        (float(img.size[1]) * float(width_percent)))
+                                    img = img.resize(
+                                        (basewidth, horizontal_size))
+                                    count_resized += 1
                         except:
-                            print(
-                                f'Could not compress {image}. Not saving it.')
+                            print(f'Could not resize {image}')
+                            count_unresized += 1
 
-    print(
-        f'resized = {count_resized}, unresized = {count_unresized}, saved={count_saved}')
+                        if img.size[0] >= 506 and img.size[1] >= 512:
+                            try:
+                                img.save(resized_image_path,
+                                         quality=88, optimize=True)
+                                count_saved += 1
+                            except:
+                                print(
+                                    f'Could not compress {image}. Not saving it.')
+
+        print(
+            f'resized = {count_resized}, unresized = {count_unresized}, saved={count_saved}')
 
 
 def main():
-    data_list = get_json()
-    data_list = prune(data_list)
-    save_data(METADATA_FILE, data_list)
-    links = get_image_links(METADATA_FILE)
-    download_images(links)
-    resize_save_images()
+    list = [1, 2, 3]
+    for number in list:
+        downloader = WikiDownloader(number)
+        metadata_file = downloader.get_paths()
+        data_list = downloader.get_json()
+        downloader.save_data(metadata_file, data_list)
+        links = downloader.get_image_links(metadata_file)
+        downloader.download_images(links)
+        downloader.resize_save_images()
 
 
 if __name__ == '__main__':
